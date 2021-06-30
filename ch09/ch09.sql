22251bc5381a88d7253822ef85fc42bb1c66ea2c
parkjaewon1 <qkrwodnjs616@naver.com>
2021-06-30 17:39:53 +0900

    2021-06-30

---
+++ ch09/ch09.sql
@@ -0,0 +1,16 @@
+-- 회원게시판에서는 작성자(대신 회원 id또는 이름), 암호, 이메일, ip가 필요 없음
+create table board (
+	num number primary key, -- key 번호
+	writer varchar2(20) not null, -- 작성자
+	subject varchar2(50) not null, -- 제목
+	content varchar2(500) not null, -- 본문
+	email varchar2(30) , -- 이메일
+	readcount number default 0, -- 읽은 횟수
+	password varchar2(12) not null, -- 암호
+	ref number not null, -- 답변글끼리 그룹
+	re_step number not null, -- ref내의 순서
+	re_level number not null, -- 들여쓰기
+	ip varchar2(20) not null, -- 작성자 ip
+	reg_date date not null, -- 작성일
+	del char(1) default 'n'
+);
---
+++ ch09/src/main/java/com/ch/ch09/model/Email.java
@@ -0,0 +1,10 @@
+package com.ch.ch09.model;
+
+import lombok.Data;
+
+@Data
+public class Email {
+	private String email;
+	private String title;
+	private String content;
+}
